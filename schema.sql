a0f191eb9f1a32050cea47d2c34d904eadca6319
nastinsk <nastinsk@gmail.com>
2019-08-03 17:20:55 -0700

    working on database

---
+++ schema.sql
@@ -0,0 +1,16 @@
+-- DROP TABLE IF EXISTS locations, weather;
+DROP TABLE IF EXISTS locations;
+
+CREATE TABLE IF NOT EXISTS locations (
+id SERIAL PRIMARY KEY,
+search_query  VARCHAR(255), 
+formatted_query VARCHAR(255), 
+latitude NUMERIC(10,7), 
+longitude NUMERIC(10,7)
+);
+
+-- CREATE TABLE IF NOT EXISTS weather (
+-- id SERIAL PRIMARY KEY,
+-- forecast TEXT,
+-- time VARCHAR(255)
+-- );
